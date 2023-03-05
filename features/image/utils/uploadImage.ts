f40575fe69ff77dc5dd7c54176cc579bfdc6fe9e
mktu <mushi9ui@gmail.com>
2023-03-05 10:54:19 +0900

    don't upload images in production

--- features/image/utils/uploadImage.ts
+++ features/image/utils/uploadImage.ts
@@ -1,4 +1,26 @@
+const uploadInProuction = (file: File) => {
+    const reader = new FileReader();
+    const promise = new Promise<string>((resolve, reject) => {
+        reader.onload = function () {
+            if (typeof reader.result === 'string') {
+                resolve(reader.result);
+            }
+            return '';
+        };
+        if (file !== null) {
+            reader.readAsDataURL(file);
+        }
+    });
+    return promise;
+};
+
+
 export const uploadImage = async (file: File) => {
+    if (process.env.NODE_ENV !== 'production') {
+        return {
+            path: await uploadInProuction(file)
+        };
+    }
     const filename = encodeURIComponent(file.name);
     const res = await fetch(`/api/upload-url?file=${filename}`);
     const { url, fields } = await res.json();
